b895458bcc64311be69b1d7d40eb66bb5a146eef
Luciano Rodriguez <luciano.rodriguez.dev@gmail.com>
2026-06-09 09:04:07 -0300

    fix: timezone para i18n

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -11,6 +11,7 @@ export default getRequestConfig(async ({ requestLocale }) => {
 
   return {
     locale,
+    timeZone: "America/Argentina/Buenos_Aires",
     messages: (await import(`../../messages/${locale}.json`)).default,
   };
 });
